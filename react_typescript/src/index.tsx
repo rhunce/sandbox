0e295efa89705e2936ecab21efe3108367e3fad1
Raymond Hunce <hunce18c@gmail.com>
2021-07-14 20:21:26 -0400

    Initiate new react context app in react_context_2 directory

--- react_typescript/src/index.tsx
+++ react_typescript/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './react_context/components/App';
+import { App } from './react_context_2/components/App'; // Modify this to show App from different directories
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ react_typescript/src/react_context_2/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const App = () => {
+  return(
+    <p>Hello World from 2!</p>
+  );
+}
